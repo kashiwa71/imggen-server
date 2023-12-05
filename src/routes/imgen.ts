1f405bd9bfc4a270378aedf17a3337357dfa1561
yuki_kashiwagi <kashiwa71@gmail.com>
2023-12-06 00:46:13 +0900

    bimg image createrに問合せる機能を追加

--- src/routes/imgen.ts
+++ src/routes/imgen.ts
@@ -1,20 +1,36 @@
 import { Router } from "express";
 import { generateImageFiles, generateImagesLinks } from "bimg";
 import { DbUtil } from "../utils/dbUtil.js";
+import { PythonShell } from "python-shell";
+
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env', debug: true });
 
 const router = Router();
 
 const dbUtil = new DbUtil();
 
+
 router.post("/links-only/:prompt", async (req: any, res: any) => {
   try {
-    const { prompt } = req.params;
-    const imageLinks = await generateImagesLinks(prompt);
+    const options: any = {
+      mode: 'text',
+      args: [process.env.BING_IMAGE_COOKIE, req.params['prompt']],
+      pythonOptions: ['-u'],
+    }
 
-    // dbに保存
-    dbUtil.insertImgenRecord(prompt, imageLinks);  
+    await PythonShell.run('src/utils/bingImageCreater_util.py', options)
+      .then((messages) => {
+        // '['hoge', 'huga']' という形式なので、JSON.parseで配列に変換
+        let messages_json: string[] = JSON.parse(messages[0].replace(/'/g, '"'));
+        // レスポンスになぜかsvgが含まれているので、それを除外
+        let url = messages_json.filter(item => !item.endsWith('.svg'));
+        return res.status(200).send(url);
+      });
 
-    return res.status(200).send(imageLinks);
+    //// dbに保存
+    //   dbUtil.insertImgenRecord(prompt, imageLinks);  
+
   } catch (err: any) {
     console.trace(err);
     return res.status(500).send(err.message);
---
+++ src/utils/bingImageCreater_util.py
@@ -0,0 +1,21 @@
+import BingImageCreator
+import sys
+import time
+
+def main(u, prompt):     
+    bingimg = BingImageCreator.ImageGen(
+        auth_cookie=u,
+        auth_cookie_SRCHHPGUSR=u,
+        quiet=True,
+    )
+
+    result_url = bingimg.get_images(prompt)
+
+    print(result_url)
+
+
+if __name__ == "__main__":
+    _U = sys.argv[1]
+    prompt = sys.argv[2]
+
+    main(_U, prompt)
